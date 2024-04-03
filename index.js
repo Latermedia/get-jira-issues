599e830ab04b1025e8bc109b365e37039279cda3
Ryan Muller <rmuller@mavrck.co>
2024-04-03 08:30:21 -0700

    Testing

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ if (!pullRequestNumber) {
 
 axios.get(githubFullUrl, { headers: prHeaders })
   .then((prDataResponse) => {
-    const prTitleCheck = prDataResponse1.data.title.match(pattern);
+    const prTitleCheck = prDataResponse.data.title.match(pattern);
     if (prTitleCheck) { 
       jiraTicketSet.add(prTitleCheck[0]);
       console.log("Added ticket from title to array: ");
